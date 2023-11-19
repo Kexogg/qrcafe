e5f806c069712771b44ca327254091593d1c8812
Kexogg <kexogg@gmail.com>
2023-11-19 23:56:51 +0500

    add new route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Login} from "./pages/Login/Login.tsx";
 import {LayoutGreen} from "./components/UI/Layout/LayoutGreen.tsx";
+import Layout from "./components/UI/Layout/Layout.tsx";
+import {CustomerHome} from "./pages/CutomerHome/CustomerHome.tsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LayoutGreen />}>
             <Route index path="/" element={<Login />}/>
+        </Route>
+          <Route path="/customer" element={<Layout />}>
+              <Route index path="home" element={<CustomerHome />}/>
         </Route>
       </Routes>
     </BrowserRouter>
