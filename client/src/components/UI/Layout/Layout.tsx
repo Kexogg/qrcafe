a7862d45c582a29a0197c2a9360a59e6783af07b
Kexogg <kexogg@gmail.com>
2023-11-20 01:49:10 +0500

    updated layout

--- client/src/components/UI/Layout/Layout.tsx
+++ client/src/components/UI/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import {CustomerNavigationMenu} from "../CustomerMenu/CustomerNavigationMenu.tsx
 const Layout = () => {
     return (
         <div className={'bg-primary-50 flex flex-col h-[100dvh]'}>
-            <main className={'grow'}>
+            <main className={'grow container mx-auto mt-5'}>
                 <Outlet/>
             </main>
             <CustomerNavigationMenu/>
