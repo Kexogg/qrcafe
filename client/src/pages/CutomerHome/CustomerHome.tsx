f5fe53b715040925362f88522a975b9537426e24
Kexogg <kexogg@gmail.com>
2023-11-25 03:57:42 +0500

    add placeholder widgets to customer home screen

--- client/src/pages/CutomerHome/CustomerHome.tsx
+++ client/src/pages/CutomerHome/CustomerHome.tsx
@@ -1,6 +1,20 @@
+import {ServedBy} from "../../components/UI/ServedBy/ServedBy.tsx";
+import {Showcase} from "../../components/UI/Showcase/Showcase.tsx";
+import {Dish} from "../../types/Dish.ts";
+import food_placeholder from "../../../public/food_placeholder.jpg";
+import {Catalog} from "../../components/UI/Catalog/Catalog.tsx";
+
 export const CustomerHome = () => {
+    const dishes = [
+        new Dish(1, 'Dish 1', 123, 'Description', '100 kg', [], food_placeholder),
+        new Dish(2, 'Dish 2', 123, 'Description', '100 kg', [], food_placeholder),
+        new Dish(3, 'Dish 3', 123, 'Description', '100 kg', [], food_placeholder),
+    ]
     return (
-        <>This is a customer home page layout
+        <><ServedBy/>
+            <Showcase title={'Новинки недели'} items={dishes}/>
+            <Showcase title={'Мы рекомендуем'} items={dishes}/>
+            <Catalog title={'Основное меню'}/>
         </>
 
     );
