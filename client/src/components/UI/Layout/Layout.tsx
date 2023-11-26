1ce2fd82400db979b9f9680abc914f9b44234585
Kexogg <kexogg@gmail.com>
2023-11-26 18:42:04 +0500

    fix height

--- client/src/components/UI/Layout/Layout.tsx
+++ client/src/components/UI/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import {CustomerNavigationMenu} from "../CustomerMenu/CustomerNavigationMenu.tsx
 
 const Layout = () => {
     return (
-        <div className={'bg-primary-50 flex flex-col h-[100dvh]'}>
+        <div className={'flex flex-col min-h-[100dvh] bg-primary-100'}>
             <main className={'grow container mx-auto mt-5'}>
                 <Outlet/>
             </main>
