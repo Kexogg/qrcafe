c64fdab66a8b20443abc9a5faf01c76847f7b7a3
Kexogg <kexogg@gmail.com>
2023-11-26 18:02:54 +0500

    add padding

--- client/src/components/UI/Showcase/Showcase.tsx
+++ client/src/components/UI/Showcase/Showcase.tsx
@@ -11,8 +11,7 @@ export const Showcase = ({title, items}: ShowcaseProps) => {
     return (
         <section>
             <h2 className={'text-accent-800 font-bold text-3xl ml-5 my-5 align-middle'}>{title}</h2>
-            <ul className={'flex gap-3 overflow-y-scroll snap-x snap-mandatory no-scrollbar cursor-grab'}>
-                <div className={'w-0 max-w-0 grow-0'}></div>
+            <ul className={'flex gap-3 overflow-y-scroll snap-x snap-mandatory no-scrollbar px-5 py-1'}>
                 {items.map((item) => (
                     <li key={item.id}
                         className={'snap-center group relative flex w-48 h-48 rounded-3xl shadow overflow-hidden shrink-0'}>
@@ -21,7 +20,6 @@ export const Showcase = ({title, items}: ShowcaseProps) => {
                         <p className={'relative text-gray-200 bg-neutral-400 bg-opacity-30 backdrop-blur-sm rounded-2xl p-3 w-fit text-right mt-auto ml-auto mb-2 mr-2 font-bold'}>{item.name}</p>
                     </li>
                 ))}
-                <div className={'w-1 max-w-1 grow-0 shrink-0'}></div>
             </ul>
         </section>
     );
