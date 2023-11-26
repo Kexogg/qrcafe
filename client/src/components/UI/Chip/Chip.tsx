81cca6ebf473c423af2297f45133d8f21881c3ca
Kexogg <kexogg@gmail.com>
2023-11-26 18:42:34 +0500

    update padding

--- client/src/components/UI/Chip/Chip.tsx
+++ client/src/components/UI/Chip/Chip.tsx
@@ -4,6 +4,6 @@ type ChipProps = {
 
 export const Chip = ({text}: ChipProps) => {
     return (
-        <li className={'p-2.5 bg-primary-700 rounded-full text-center text-white font-medium text-lg inline-block'}>{text}</li>
+        <li className={'py-1 px-3 bg-primary-700 rounded-full text-center text-white font-medium text-lg inline-block'}>{text}</li>
     );
 };
