1e6ea085f4cc4680cebf1eee4244b95a058afb53
Kexogg <kexogg@gmail.com>
2023-12-06 18:44:10 +0500

    localstorage

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,13 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
-import {cartSlice} from "./features/cart/cartSlice.ts";
-import {waiterSlice} from "./features/waiter/waiterSlice.ts";
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { cartSlice } from './features/cart/cartSlice.ts'
+import { waiterSlice } from './features/waiter/waiterSlice.ts'
+
+function saveToLocalStorage(state: RootState) {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    } catch (e) {
+        console.warn(e)
+    }
+}
+
+function loadFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem('state')
+        if (serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    } catch (e) {
+        console.warn(e)
+        return undefined
+    }
+}
+
+const preloadedState = loadFromLocalStorage()
+
+const reducers = combineReducers({
+    cart: cartSlice.reducer,
+    waiter: waiterSlice.reducer,
+})
 
 export const store = configureStore({
-    reducer: {
-        cart: cartSlice.reducer,
-        waiter: waiterSlice.reducer
-    }
+    reducer: reducers,
+    preloadedState,
 })
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export type RootState = ReturnType<typeof store.getState>
 
