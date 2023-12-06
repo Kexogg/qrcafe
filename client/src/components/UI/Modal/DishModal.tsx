71f82dedd3a16c546a8632b5207dccb22ae0c857
Kexogg <kexogg@gmail.com>
2023-12-06 18:55:11 +0500

    fix count bug

--- client/src/components/UI/Modal/DishModal.tsx
+++ client/src/components/UI/Modal/DishModal.tsx
@@ -1,85 +1,153 @@
-import {ArrowBackRounded, FavoriteBorderOutlined} from "@mui/icons-material";
-import Modal from "./Modal.tsx";
-import {getDishTotal, IDish, toggleDishExtra} from "../../../types/IDish.ts";
-import {useEffect, useState} from "react";
-import {Button} from "../Button/Button.tsx";
-import {addToCart, updateCart} from "../../../features/cart/cartSlice.ts";
-import {useAppDispatch} from "../../../hooks.ts";
-import {CountInput} from "../CountInput/CountInput.tsx";
+import { ArrowBackRounded, FavoriteBorderOutlined } from '@mui/icons-material'
+import Modal from './Modal.tsx'
+import { getDishTotal, IDish, toggleDishExtra } from '../../../types/IDish.ts'
+import { useEffect, useState } from 'react'
+import { Button } from '../Button/Button.tsx'
+import { addToCart, updateCartItem } from '../../../features/cart/cartSlice.ts'
+import { useAppDispatch } from '../../../hooks.ts'
+import { CountInput } from '../CountInput/CountInput.tsx'
 
 type DishModalProps = {
-    dish: IDish | null;
-    onClose: () => void;
-    isInCart?: boolean;
+    dish: IDish | null
+    onClose: () => void
+    isInCart?: boolean
 }
 
-const DishModal = ({dish, onClose, isInCart}: DishModalProps) => {
-    const [currentDish, setCurrentDish] = useState<IDish | null>(null);
+const DishModal = ({ dish, onClose, isInCart }: DishModalProps) => {
+    const [currentDish, setCurrentDish] = useState<IDish | null>(null)
     useEffect(() => {
-        setCurrentDish(dish ? {...structuredClone(dish), count: 1} : null)
-    }, [dish]);
+        setCurrentDish(
+            dish ? { ...structuredClone(dish), count: dish.count ?? 1 } : null,
+        )
+    }, [dish])
     const dispatch = useAppDispatch()
     return (
         <Modal open={!!dish} onClose={onClose}>
-            {
-                currentDish &&
-                <div className={'flex flex-col gap-3 h-full'}>
+            {currentDish && (
+                <div className={'flex h-full flex-col gap-3'}>
                     <div className={'relative flex aspect-[3/2] shrink-0'}>
-                        <img src={currentDish.image} alt={currentDish.name}
-                             className={'absolute w-full aspect-[3/2] object-cover rounded-3xl'}/>
-                        <nav className={'relative w-full h-fit flex justify-between p-3'}>
-                            <button className={'p-3 bg-white/25 rounded-full backdrop-blur text-white'}
-                                    onClick={onClose}>
-                                <ArrowBackRounded fontSize={'large'}/>
+                        <img
+                            src={currentDish.image}
+                            alt={currentDish.name}
+                            className={
+                                'absolute aspect-[3/2] w-full rounded-3xl object-cover'
+                            }
+                        />
+                        <nav
+                            className={
+                                'relative flex h-fit w-full justify-between p-3'
+                            }>
+                            <button
+                                className={
+                                    'rounded-full bg-white/25 p-3 text-white backdrop-blur'
+                                }
+                                onClick={onClose}>
+                                <ArrowBackRounded fontSize={'large'} />
                             </button>
-                            <button className={'p-3 bg-white/25 rounded-full backdrop-blur text-white'}>
-                                <FavoriteBorderOutlined fontSize={'large'}/>
+                            <button
+                                className={
+                                    'rounded-full bg-white/25 p-3 text-white backdrop-blur'
+                                }>
+                                <FavoriteBorderOutlined fontSize={'large'} />
                             </button>
                         </nav>
                     </div>
                     <section className={'min-h-0 grow overflow-y-scroll'}>
-                        <h1 className={'text-primary-700 text-3xl font-bold'}>{currentDish.name}</h1>
-                        <p className={'text-primary-700 text-lg'}>{currentDish.weight}</p>
-                        <p className={'text-primary-500 text-lg'}>{currentDish.description}</p>
+                        <h1 className={'text-3xl font-bold text-primary-700'}>
+                            {currentDish.name}
+                        </h1>
+                        <p className={'text-lg text-primary-700'}>
+                            {currentDish.weight}
+                        </p>
+                        <p className={'text-lg text-primary-500'}>
+                            {currentDish.description}
+                        </p>
                     </section>
-                    <section className={'flex flex-col mt-auto gap-3 grow-0 justify-end'}>
-                        {
-                            currentDish.extras.length > 0 &&
+                    <section
+                        className={
+                            'mt-auto flex grow-0 flex-col justify-end gap-3'
+                        }>
+                        {currentDish.extras.length > 0 && (
                             <>
-                                <h2 className={'text-primary-700 text-xl font-medium'}>Добавить</h2>
-                                <ul className={'flex gap-3 overflow-y-scroll grow-0 no-scrollbar pb-1'}>
+                                <h2
+                                    className={
+                                        'text-xl font-medium text-primary-700'
+                                    }>
+                                    Добавить
+                                </h2>
+                                <ul
+                                    className={
+                                        'no-scrollbar flex grow-0 gap-3 overflow-y-scroll pb-1'
+                                    }>
                                     {currentDish.extras.map((extra) => {
                                         return (
                                             <li key={extra.id}>
                                                 <button
-                                                    className={`whitespace-nowrap border-2 p-2 border-primary-700 rounded-3xl ${extra.applied ? 'bg-primary-700 text-white' : 'text-primary-700'}`}
+                                                    className={`whitespace-nowrap rounded-3xl border-2 border-primary-700 p-2 ${
+                                                        extra.applied
+                                                            ? 'bg-primary-700 text-white'
+                                                            : 'text-primary-700'
+                                                    }`}
                                                     onClick={() => {
-                                                        setCurrentDish({...currentDish, extras: toggleDishExtra(currentDish, extra.id)})
+                                                        setCurrentDish({
+                                                            ...currentDish,
+                                                            extras: toggleDishExtra(
+                                                                currentDish,
+                                                                extra.id,
+                                                            ),
+                                                        })
                                                     }}>
-                                                    {extra.name}<br/>+{extra.price}₽
+                                                    {extra.name}
+                                                    <br />+{extra.price}₽
                                                 </button>
                                             </li>
                                         )
                                     })}
                                 </ul>
                             </>
-                        }
+                        )}
                         <div className={'flex items-center'}>
-                            <CountInput onCountChange={(count) => setCurrentDish({...currentDish, count: count})}
-                                        count={currentDish.count ?? 1}/>
-                            <span className={'text-xl font-bold text-primary-700 ml-auto mr-3'}>Итого:</span>
-                            <p className={'rounded-full bg-primary-700 text-white py-2.5 px-4 text-lg font-semibold '}>{getDishTotal(currentDish)}₽</p>
+                            <CountInput
+                                onCountChange={(count) =>
+                                    setCurrentDish({
+                                        ...currentDish,
+                                        count: count,
+                                    })
+                                }
+                                count={currentDish.count ?? 1}
+                            />
+                            <span
+                                className={
+                                    'ml-auto mr-3 text-xl font-bold text-primary-700'
+                                }>
+                                Итого:
+                            </span>
+                            <p
+                                className={
+                                    'rounded-full bg-primary-700 px-4 py-2.5 text-lg font-semibold text-white '
+                                }>
+                                {getDishTotal(currentDish)}₽
+                            </p>
                         </div>
-                        <Button label={isInCart ? 'Подтвердить изменения' : 'Добавить в заказ'} dark onClick={() => {
-                            if (isInCart) dispatch(updateCart(currentDish))
-                            else dispatch(addToCart(currentDish));
-                            onClose();
-                        }}/>
+                        <Button
+                            label={
+                                isInCart
+                                    ? 'Подтвердить изменения'
+                                    : 'Добавить в заказ'
+                            }
+                            dark
+                            onClick={() => {
+                                if (isInCart)
+                                    dispatch(updateCartItem(currentDish))
+                                else dispatch(addToCart(currentDish))
+                                onClose()
+                            }}
+                        />
                     </section>
                 </div>
-            }
+            )}
         </Modal>
-    );
-};
+    )
+}
 
-export default DishModal;
+export default DishModal
