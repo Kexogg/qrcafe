d9ab00983dfee0708c4b9a7754f99ea43e36fcdc
Kexogg <kexogg@gmail.com>
2023-12-07 04:09:38 +0500

    add disabled state

--- client/src/components/UI/CountInput/CountInput.tsx
+++ client/src/components/UI/CountInput/CountInput.tsx
@@ -1,38 +1,49 @@
-import {AddRounded, RemoveRounded} from "@mui/icons-material";
-import {useState} from "react";
+import { AddRounded, RemoveRounded } from '@mui/icons-material'
+import { useState } from 'react'
 
 type CountInputProps = {
-    onCountChange: (count: number) => void;
-    min?: number;
-    max?: number;
-    count: number;
+    onCountChange: (count: number) => void
+    min?: number
+    max?: number
+    count: number
+    disabled?: boolean
 }
 
-export const CountInput = ({onCountChange, min, max, count}: CountInputProps) => {
-    const [currentCount, setCurrentCount] = useState(count);
+export const CountInput = ({
+    onCountChange,
+    min,
+    max,
+    count,
+    disabled,
+}: CountInputProps) => {
+    const [currentCount, setCurrentCount] = useState(count)
 
     return (
-        <div className={'flex items-center h-full'}>
+        <div className={'flex h-full items-center'}>
             <button
-                disabled={count <= (min ?? 1) }
+                disabled={count <= (min ?? 1) || disabled}
                 onClick={() => {
                     onCountChange(count - 1)
                     setCurrentCount(count - 1)
                 }}
-                className={`disabled:bg-primary-400 bg-primary-700 h-full text-white p-2 rounded-full aspect-square`}>
-                <RemoveRounded/>
+                className={`aspect-square h-full rounded-full bg-primary-700 p-2 text-white disabled:bg-primary-400`}>
+                <RemoveRounded />
             </button>
             <span
-                className={'font-semibold text-xl text-primary-700 mx-3 text-center'}>{currentCount}</span>
+                className={
+                    'mx-3 text-center text-xl font-semibold text-primary-700'
+                }>
+                {currentCount}
+            </span>
             <button
-                disabled={count >= (max ?? 20)}
+                disabled={count >= (max ?? 20) || disabled}
                 onClick={() => {
                     onCountChange(count + 1)
                     setCurrentCount(count + 1)
                 }}
-                className={`disabled:bg-primary-400 bg-primary-700 h-full text-white p-2 rounded-full aspect-square`}>
-                <AddRounded/>
+                className={`aspect-square h-full rounded-full bg-primary-700 p-2 text-white disabled:bg-primary-400`}>
+                <AddRounded />
             </button>
         </div>
-    );
-};
+    )
+}
