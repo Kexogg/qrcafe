a281d6c1220b41b0639c6a43353f62462af6e0db
Kexogg <kexogg@gmail.com>
2023-12-07 04:45:09 +0500

    Confirmation modal

--- client/src/pages/CustomerSettings/CustomerSettings.tsx
+++ client/src/pages/CustomerSettings/CustomerSettings.tsx
@@ -2,20 +2,37 @@ import { BuildInfo } from '../../components/BuildInfo/BuildInfo.tsx'
 import { PageTitle } from '../../components/UI/PageTitle/PageTitle.tsx'
 import { useAppSelector } from '../../hooks.ts'
 import { Button } from '../../components/UI/Button/Button.tsx'
+import Modal from '../../components/UI/Modal/Modal.tsx'
+import { useState } from 'react'
 
 export const CustomerSettings = () => {
     const state = useAppSelector((state) => state)
+    const [modalOpen, setModalOpen] = useState(false)
     return (
         <section className={'flex flex-col gap-5'}>
+            <Modal
+                autoHeight
+                title={'Вы уверены, что хотите выйти?'}
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}>
+                <p>
+                    При выходе из приложения данные будут безвозвратно утеряны
+                </p>
+                <Button
+                    dark
+                    label={'Выйти'}
+                    onClick={() => {
+                        localStorage.clear()
+                        window.location.href = '/'
+                    }}
+                />
+            </Modal>
             <PageTitle title={'Настройки'} />
             <span className={'flex max-w-sm flex-col gap-2'}>
                 <Button
                     label={'Удалить данные и выйти'}
                     dark
-                    onClick={() => {
-                        localStorage.clear()
-                        window.location.href = '/'
-                    }}
+                    onClick={() => setModalOpen(true)}
                 />
                 <Button
                     label={'Перезагрузить страницу'}
