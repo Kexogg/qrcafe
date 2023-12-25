a63652dd4b125e41f4e9835185f44a65391ee0c4
Артём <amatevosan674@gmail.com>
2023-12-25 18:30:48 +0500

    Migrations

---
+++ server/API/Migrations/20231218181402_EmployeeAuth.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QrCafe.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "employees");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "employees");
+        }
+    }
+}
---
+++ server/API/Migrations/20231224102607_CategoryOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QrCafe.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "num",
+                table: "categories",
+                newName: "order");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "order",
+                table: "categories",
+                newName: "num");
+        }
+    }
+}
