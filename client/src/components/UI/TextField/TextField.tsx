3223b142983f4645ee056362bb0e1fceb02d3ccf
Kexogg <kexogg@gmail.com>
2024-01-02 16:00:06 +0500

    update type

--- client/src/components/UI/TextField/TextField.tsx
+++ client/src/components/UI/TextField/TextField.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-
-type TextFieldProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
-    dark?: boolean;
+type TextFieldProps = React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+> & {
+    dark?: boolean
 }
 
-const TextField = ({ dark, ...props}: TextFieldProps) => {
-    let colorClass: string;
+const TextField = ({ dark, ...props }: TextFieldProps) => {
+    let colorClass: string
     if (dark) {
-        colorClass = 'border-primary-700 focus:border-primary-800 hover:border-primary-800 text-primary-800';
+        colorClass =
+            'border-primary-700 focus:border-primary-800 hover:border-primary-800 text-primary-800'
     } else {
-        colorClass = 'border-primary-300 focus:border-primary-200 hover:border-primary-200 text-primary-200';
+        colorClass =
+            'border-primary-300 focus:border-primary-200 hover:border-primary-200 text-primary-200'
     }
     return (
-        <input {...props} type={"text"} className={`${colorClass} no-ring transition-colors border-b-2 p-2 bg-transparent placeholder-primary-400`}>
+        <input
+            type={'text'}
+            {...props}
+            className={`${colorClass} no-ring border-b-2 bg-transparent p-2 placeholder-primary-400 transition-colors`}></input>
+    )
+}
 
-        </input>
-    );
-};
-
-export default TextField;
+export default TextField
