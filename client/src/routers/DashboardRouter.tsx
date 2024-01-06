ff3365b6797f611ad04f311e338e289beffae785
Kexogg <kexogg@gmail.com>
2024-01-06 16:01:45 +0500

    Add dashboard stub

--- client/src/routers/DashboardRouter.tsx
+++ client/src/routers/DashboardRouter.tsx
@@ -1,3 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { DashboardHome } from '../pages/Dashboard/DashboardHome/DashboardHome.tsx'
+import { LayoutDashboard } from '../components/UI/Layout/LayoutDashboard.tsx'
+
 export const DashboardRouter = () => {
-    return <></>
+    return (
+        <Routes>
+            <Route path="/dashboard" element={<LayoutDashboard />}>
+                <Route index path="home" element={<DashboardHome />} />
+            </Route>
+            <Route
+                path="*"
+                element={<Navigate replace to={'/dashboard/home'} />}
+            />
+        </Routes>
+    )
 }
