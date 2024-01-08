025dd462e9b78b81dac5fd5f8c41af63dc6c18b3
Kexogg <kexogg@gmail.com>
2024-01-08 19:55:51 +0500

    update employee interface

--- client/src/types/IEmployee.ts
+++ client/src/types/IEmployee.ts
@@ -1,7 +1,22 @@
+export enum EmployeeRole {
+    ADMIN = 0,
+    WAITER = 1,
+}
 export interface IEmployee {
     id: string
     login: string
+    password?: string
     fullName: string
     role: number
-    available: string
+    available: boolean
+}
+
+export const getEmployeeStub = (): IEmployee => {
+    return {
+        id: '',
+        login: '',
+        fullName: '',
+        role: 1,
+        available: false,
+    }
 }
