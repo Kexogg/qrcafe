927353c6d55ad2dfcf2c96710214fa902c0219aa
Kexogg <kexogg@gmail.com>
2024-01-09 16:03:15 +0500

    add orders

--- client/src/pages/Dashboard/DashboardOrders/DashboardOrders.tsx
+++ client/src/pages/Dashboard/DashboardOrders/DashboardOrders.tsx
@@ -1,3 +1,17 @@
+import { DashboardPageTemplate } from '../DashboardPageTemplate/DashboardPageTemplate.tsx'
+import { getOrders } from '../../../api/api.ts'
+
 export const DashboardOrders = () => {
-    return <></>
+    return (
+        <DashboardPageTemplate
+            pageTitle={'Заказы'}
+            getItems={getOrders}
+            tableColumns={[
+                {
+                    name: 'Название',
+                    key: 'name',
+                },
+            ]}
+        />
+    )
 }
