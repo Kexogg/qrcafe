1707bc6db64cc8e8120a03738ba29214cd990029
Артём <amatevosan674@gmail.com>
2024-01-12 02:14:42 +0500

    S3 bucket integration

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -88,7 +88,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<QrCafeDbContext>();
     context.Database.EnsureDeleted();
-    context.Database.EnsureCreated();
+    //context.Database.EnsureCreated();
     context.Database.Migrate();
 }
 
